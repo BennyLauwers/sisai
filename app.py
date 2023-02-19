08c481941e3cfb0dc2585298c1a081d004066db5
Benny Lauwers <BennyLauwers@users.noreply.github.com>
2023-02-19 20:04:46 +0100

    g

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def index():
     return render_template('index.html', len = len(camera_list), full_rows = amount_full_rows, todays_alert_values=array_todays_alert_values, delta = array_delta_values, camera_list = camera_list, today_formatted=today_formatted)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0")
